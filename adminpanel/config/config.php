fe486ca1ac84d8766258a8d5a3b260bc66f03fa4
Khangb1910527 <khangb1910527@student.ctu.edu.vn>
2023-05-30 11:33:18 +0700

    khang them trang chu

--- adminpanel/config/config.php
+++ adminpanel/config/config.php
@@ -8,7 +8,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=batdongsan", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "đc r nhoc";
+  echo "";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
--- content/main.php
+++ content/main.php
@@ -1,5 +1,3 @@
-
-
 <div id="main">
             <div class="maincontent">
                 <?php
